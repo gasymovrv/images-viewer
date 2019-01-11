7d631e5b9ea79c71a073c88d134423bc2148b8b4
04gas90@mail.ru <04gas90@mail.ru>
2019-01-11 17:14:14 +0400

    подгрузка объектов по скроллу вместо пэйджинга

--- imgviewer-front/src/components/ViewerBox/ViewerBox.js
+++ imgviewer-front/src/components/ViewerBox/ViewerBox.js
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react';
-import Pagination from 'react-js-pagination';
 import {findFilesByDirectoryId} from "../../api/filesApi";
-import {Button, Container, Row} from 'reactstrap';
+import {Badge, Button, Col, Container, Row} from 'reactstrap';
 import Item from '../Item';
 
 /**
@@ -19,6 +18,8 @@ export default class ViewerBox extends React.Component {
         }
         this.state = {
             dirId: dirId,
+            loadAppend: false,
+            fullLoad: false,
             zoom: 4,
             files:[],
             activePage: 1,
@@ -26,11 +27,6 @@ export default class ViewerBox extends React.Component {
             totalItemsCount: 0
         };
     }
-
-    handlePageChange = (pageNumber) => {
-        this.setState({activePage: pageNumber});
-        this.loadFilesWithPaging(pageNumber, this.state.itemsCountPerPage);
-    };
 
     handleZoomPlus = () => {
         this.setState((prSt)=>{
@@ -45,43 +41,93 @@ export default class ViewerBox extends React.Component {
     handleZoomMinus = () => {
         this.setState((prSt)=>{
             let resultZoom = prSt.zoom-1;
-            if(resultZoom<1){
+            if(resultZoom<4){
                 resultZoom++;
             }
             return {zoom: resultZoom}
         });
     };
 
+    handleScroll = () => {
+        const scrollHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+        console.log(scrollHeight);
+        //Когда прокрутили до самого низа (-50 - чтобы сработало не совсем внизу)
+        if (window.pageYOffset >= (scrollHeight - document.documentElement.clientHeight) - 50 && !this.state.loadAppend && !this.state.fullLoad) {
+            this.setState((prevState) => ({
+                activePage: prevState.activePage + 1,
+                loadAppend: true
+            }));
+        }
+    };
+
     componentDidMount(){
         const {activePage, itemsCountPerPage} = this.state;
-        this.loadFilesWithPaging(activePage, itemsCountPerPage)
+        this.loadFiles(activePage, itemsCountPerPage);
+        window.addEventListener('scroll', this.handleScroll);
     }
 
     componentDidUpdate(prevProps, prevState){
         const {match} = this.props;
+        const {loadAppend, activePage, itemsCountPerPage} = this.state;
         if (match && match.params && match.params.id && parseInt(match.params.id) !== prevState.dirId) {
-            const {activePage, itemsCountPerPage} = this.state;
             this.setState({
                 dirId: parseInt(match.params.id),
                 activePage: 1
             });
-            this.loadFilesWithPaging(activePage, itemsCountPerPage)
+            this.loadFiles(activePage, itemsCountPerPage)
+        } else if(loadAppend && !prevState.loadAppend && activePage !== prevState.activePage){
+            this.loadFilesWithAppend(activePage, itemsCountPerPage)
         }
     }
 
-    loadFilesWithPaging = (activePage, itemsCountPerPage) => {
-        const {match} = this.props;
+    loadFilesWithAppend = (activePage, itemsCountPerPage) => {
+        const {dirId} = this.state;
         findFilesByDirectoryId(
             (entities, totalElements) => {
-                this.setState({files: [...entities], totalItemsCount: totalElements})
+                this.setState((prevState) => {
+                    if (entities && entities.length>0) {
+                        return {
+                            files: [...prevState.files, ...entities],
+                            totalItemsCount: totalElements,
+                            loadAppend: false,
+                            fullLoad: false
+                        }
+                    } else {
+                        return {
+                            activePage: activePage-1,
+                            loadAppend: false,
+                            fullLoad: true
+                        }
+                    }
+                })
             },
-            match.params.id,
+            dirId,
+            activePage,
+            itemsCountPerPage)
+    };
+
+    loadFiles = (activePage, itemsCountPerPage) => {
+        const {dirId} = this.state;
+        findFilesByDirectoryId(
+            (entities, totalElements) => {
+                this.setState({
+                    files: [...entities],
+                    totalItemsCount: totalElements,
+                    loadAppend: false,
+                    fullLoad: entities.length === 0
+                })
+            },
+            dirId,
             activePage,
             itemsCountPerPage)
     };
 
     render() {
-        const {files, activePage, itemsCountPerPage, totalItemsCount, zoom} = this.state;
+        const {files, totalItemsCount, zoom} = this.state;
         const imgsOrVideos = files.map((file)=>{
             return <Item key={file.id} file={file} zoom={zoom}/>
         });
@@ -98,25 +144,23 @@ export default class ViewerBox extends React.Component {
                 <Row>
                     <section className="gallery-block grid-gallery">
                         <Container>
+                            <Row style={{marginBottom: 'calc(2vh)'}}>
+                                <Col xs="12">
+                                    <h5 style={{display:'inline'}}>Всего объектов: <Badge color="secondary">{totalItemsCount}</Badge></h5>
+                                    <h5 style={{display:'inline'}}> Показано объектов: <Badge color="secondary">{files.length}</Badge></h5>
+                                </Col>
+                            </Row>
                             <Row>
                                 {imgsOrVideos}
                             </Row>
                         </Container>
                     </section>
                 </Row>
-                <Row>
-                    <Pagination
-                        itemClass="page-item"
-                        linkClass="page-link"
-                        aria-label="Page navigation example"
-                        activePage={activePage}
-                        itemsCountPerPage={itemsCountPerPage}
-                        totalItemsCount={totalItemsCount}
-                        pageRangeDisplayed={5}
-                        onChange={this.handlePageChange}
-                    />
-                </Row>
             </Fragment>
         )
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll);
     }
 }
